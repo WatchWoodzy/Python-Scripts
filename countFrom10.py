7404827efaf0d254b3cc31d3cb4b563083daafe6
WatchWoodzy <treeckogamingfun@gmail.com>
2022-10-25 09:05:33 +0800

    up to 10 and from 10

---
+++ countFrom10.py
@@ -0,0 +1,15 @@
+#
+#
+#
+#
+#
+
+def countDown():
+    start = 11
+    for x in range(10):
+        start = start - 1
+        print(start)
+
+
+
+countDown()
---
+++ countTo10.py
@@ -0,0 +1,15 @@
+#
+#
+#
+#
+#
+
+def countUp():
+    start = 0
+    for x in range(10):
+        start = start + 1
+        print(start)
+
+
+
+countUp()
